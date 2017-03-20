a0dd291fc3e93487ae4794b7f26215619f2f3578
RuppyRup <rupert.waldron@yahoo.co.uk>
2017-03-20 14:28:43 +0000

    Added story board and data model and some table controls

---
+++ MyHood/Post.swift
@@ -0,0 +1,22 @@
+//
+//  Post.swift
+//  MyHood
+//
+//  Created by Rupert Waldron on 20/03/2017.
+//  Copyright © 2017 Rupert Waldron. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    
+    fileprivate var imagePath: String
+    fileprivate var title: String
+    fileprivate var postDesc: String
+    
+    init(imagePath: String, title: String, description: String) {
+        self.imagePath = imagePath
+        self.title = title
+        self.postDesc = description
+    }
+}
